914eac0b1d1f521ef14187dca0bcea828067eb32
14065 <panther011216@yahoo.co.jp>
2017-10-25 23:11:48 +0900

    phpでメイン,履歴,画像登録のページの作成.また,画像のみを表示するプログラムとMySQLの情報に関するプログラムの作成

---
+++ history.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>履歴ページ</title>
+</head>
+<body>
+<table border="1" width="65%">
+    <tr>
+        <th>ID</th>
+        <th>Todo</th>
+        <th>Doing</th>
+        <th>Done</th>
+        <th>貼った画像</th>
+        <th>貼った時間</th>
+    </tr>
+<?php
+require 'common.php';
+
+try{
+  $pdo = connect_db();
+
+  $sql = 'SELECT * FROM images';
+  $stmt = $pdo->query($sql);
+  if(!$stmt){
+    $info = $pdo->errorInfo();
+    exit($info[2]);
+  }
+  $row_count = $stmt->rowCount();
+  echo 'レコード件数: ' .$row_count. "<br>";
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    echo '<tr align=center>';
+    echo '<td>' .$row['id']. '</td>';
+    echo '<td>' .$row['todo']. '</td>';
+    echo '<td>' .$row['doing']. '</td>';
+    echo '<td>' .$row['done']. '</td>';
+?>
+    <td><img src="img_show.php?id=<?php echo $row['id'];?> width="480" height="270""></td>
+
+<?php
+    echo '<td>' .$row['time']. '</td>';
+    echo '</tr>';
+  }
+  echo '</table>';
+  $pdo = null;
+}catch(PDOException $e){
+  print('Error: ' .$e->getMessage());
+}
+?>
+
+<a href="main.php"><input type="button" value="戻る">
+</a>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+<title>メイン画面</title>
+</head>
+<body>
+<?php
+require 'common.php';
+try{
+  $pdo = connect_db();
+  $sql = 'SELECT * FROM images WHERE id=(SELECT MAX(ID) FROM images)';
+  $stmt = $pdo->query($sql);
+  if(!$stmt){
+    $info = $pdo->errorInfo();
+    exit($info[2]);
+  }
+
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+?>
+    <img src="img_show.php?id=<?php echo $row['id']; ?> width="640" height="480""><br>
+<?php
+    echo 'Todoの付箋の枚数 : ' .$row['todo']. '枚  <br>';
+    echo 'Doingの付箋の枚数: ' .$row['doing']. '枚  <br>';
+    echo 'Doneの付箋の枚数 : ' .$row['done']. '枚  <br>';
+    }
+  $pdo = null;
+}catch(PDOException $e){
+  print('Error: ') .$e->getMessage();
+}
+?>
+
+<br><br><br>
+<a href="img_up.php"><input type="button" value="画像を登録する"></a>
+
+<a href="test.php"><input type="button" value="今までの履歴を見る"></a>
+</body>
+</html>
